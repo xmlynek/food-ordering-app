80fe9bce6e061d0a24dcfb8fdfa6f46c47bbf5c3
Filip Mlýnek <mlynekff@gmail.com>
2024-04-02 18:59:04 +0200

    Modify payment command responses

--- common/src/main/java/com/food/ordering/app/common/response/payment/ProcessPaymentFailed.java
+++ common/src/main/java/com/food/ordering/app/common/response/payment/ProcessPaymentFailed.java
@@ -1,10 +1,11 @@
 package com.food.ordering.app.common.response.payment;
 
+import com.food.ordering.app.common.enums.PaymentStatus;
 import java.util.UUID;
 import lombok.Builder;
 
 @Builder
-public record ProcessPaymentFailed(UUID orderId, UUID customerId, String failureMessage) implements
-    ProcessPaymentResponse {
+public record ProcessPaymentFailed(UUID orderId, UUID customerId, PaymentStatus paymentStatus,
+                                   String failureMessage) implements ProcessPaymentResponse {
 
 }
--- common/src/main/java/com/food/ordering/app/common/response/payment/ProcessPaymentSucceeded.java
+++ common/src/main/java/com/food/ordering/app/common/response/payment/ProcessPaymentSucceeded.java
@@ -1,10 +1,11 @@
 package com.food.ordering.app.common.response.payment;
 
+import com.food.ordering.app.common.enums.PaymentStatus;
 import java.util.UUID;
 import lombok.Builder;
 
 @Builder
-public record ProcessPaymentSucceeded(UUID paymentId, UUID orderId) implements
-    ProcessPaymentResponse {
+public record ProcessPaymentSucceeded(UUID paymentId, UUID orderId,
+                                      PaymentStatus paymentStatus) implements ProcessPaymentResponse {
 
 }
